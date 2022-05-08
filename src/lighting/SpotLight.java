c4281ffc594dbbab982b976b2e29615f8ad0580a
Aryeh Shebson <aryehshebson@gmail.com>
2022-05-08 21:48:01 +0300

    afvdfbadfb

--- src/lighting/SpotLight.java
+++ src/lighting/SpotLight.java
@@ -5,11 +5,13 @@ public class SpotLight extends PointLight {
     private Vector direction;
     public SpotLight(Color intensity, Point position, Vector direction) {
         super(intensity, position);
-        this.direction = direction;
+        this.direction = direction.normalize();
     }
 
     @Override
     public Color getIntensity(Point p) {
-        return super.getIntensity(p).scale(Math.max(0, direction.dotProduct(getL(p))));
+        Color I0 = super.getIntensity(p);
+        Vector l = this.getL(p);
+        return I0.scale(Math.max(0, direction.dotProduct(l)));
     }
 }
