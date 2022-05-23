b74bb43c052f6c4bcd6ed1a6c0f6c7f686d045dd
Zvi Korach <zvikora@gmail.com>
2022-05-23 18:07:37 +0300

    soft shadows tests

--- src/integrationTests/SoftShadowsTests.java
+++ src/integrationTests/SoftShadowsTests.java
@@ -48,12 +48,38 @@ public class SoftShadowsTests {
 						.setMaterial(new Material().setKD(0.5).setKS(0.5).setNShininess(60)), //
 				new Sphere(new Point(60, 50, -50), 30d).setEmission(new Color(BLUE)) //
 						.setMaterial(new Material().setKD(0.2).setKS(0.2).setNShininess(30).setKT(0.4)));
-		scene.lights.add(new LightBulb(new Color(700, 400, 400), new Point(60, 50, -50), 20d) //
+		scene.lights.add(new LightBulb(new Color(700, 400, 400), new Point(60, 50, 0), 10) //
 				.setKL(4E-5).setKQ(2E-7));
 
 		ImageWriter imageWriter = new ImageWriter("softShadowRefraction", 600, 600);
 		camera.setImageWriter(imageWriter) //
 				.setRayTracer(new RayTracerBasic(scene)) //
+				.renderImage() //
+				.writeToImage();
+	}
+
+	@Test
+	public void softShadowtrianglesSphere() {
+		Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+			.setWidthAndHeight(200, 200).setDistance(1000) //
+			.setRayTracer(new RayTracerBasic(scene));
+
+		scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
+
+		scene.geometries.add( //
+				new Triangle(new Point(-150, -150, -115), new Point(150, -150, -135), new Point(75, 75, -150)) //
+						.setMaterial(new Material().setKS(0.8).setNShininess(60)), //
+				new Triangle(new Point(-150, -150, -115), new Point(-70, 70, -140), new Point(75, 75, -150)) //
+						.setMaterial(new Material().setKS(0.8).setNShininess(60)), //
+				new Sphere(new Point(0, 0, -11), 30d) //
+						.setEmission(new Color(java.awt.Color.BLUE)) //
+						.setMaterial(new Material().setKD(0.5).setKS(0.5).setNShininess(30)) //
+		);
+		scene.lights.add( //
+				new LightBulb(new Color(700, 400, 400), new Point(40, 40, 115), 10) //
+						.setKL(4E-4).setKQ(2E-5));
+
+		camera.setImageWriter(new ImageWriter("softShadowTrianglesSphere", 600, 600)) //
 				.renderImage() //
 				.writeToImage();
 	}
--- src/renderer/RayTracerBasic.java
+++ src/renderer/RayTracerBasic.java
@@ -14,7 +14,7 @@ public class RayTracerBasic extends RayTracerBase {
     private static final int MAX_CALC_COLOR_LEVEL = 10;
     private static final Double3 INITIAL_K = Double3.ONE;
     private static final double MIN_CALC_COLOR_K = 0.0001;
-    private static final int MAX_NUMBER_OF_SHADOW_RAYS = 100;
+    private static final int MAX_NUMBER_OF_SHADOW_RAYS = 1000;
 
     /**
      * Constructor
